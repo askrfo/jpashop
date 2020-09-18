3c89fea5a69641b15a3b18c47db698d17aae6764
김정덕 <jeonkim@ebay.com>
2020-09-18 18:47:24 +0900

    엔티티

---
+++ src/main/java/jpabook/jpashop/domain/OrderItem.java
@@ -0,0 +1,27 @@
+package jpabook.jpashop.domain;
+
+import javax.persistence.*;
+import jpabook.jpashop.domain.item.Item;
+import lombok.Getter;
+import lombok.Setter;
+
+@Entity
+@Table(name = "order_item")
+@Getter @Setter
+public class OrderItem {
+
+  @Id @GeneratedValue
+  @Getter @Setter
+  private Long id;
+
+  @ManyToOne(fetch = FetchType.LAZY)
+  @JoinColumn(name = "item_id")
+  private Item item;
+
+  @ManyToOne(fetch = FetchType.LAZY)
+  @JoinColumn(name = "order_id")
+  private Order order;
+
+  private int orderPrice;
+  private int count;
+}
